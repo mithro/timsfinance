e280c3c9869fa9f3149287258120934d84379f73
Tim 'mithro' Ansell <mithro@mithis.com>
2012-02-12 19:56:47 +1100

    Fixing the start/end date handling.

--- finance/management/commands/details.py
+++ finance/management/commands/details.py
@@ -23,10 +23,13 @@ class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
         make_option(
             "--start", action="store", dest="start_date",
-            help="Start date.", default=datetime.now().replace(day=1, hour=0, minute=0, second=0, microsecond=0)),
+            help="Start date.", default=None),
         make_option(
             "--end", action="store", dest="end_date",
-            help="End date.", default=datetime.now().replace(day=31, hour=23, minute=59, second=59, microsecond=999999)),
+            help="End date.", default=None),
+        make_option(
+            "--level", type="int", dest="level",
+            help="Number of levels deep to print.", default=999),
         make_option(
             "--accounts", action="append", dest="accounts",
             help="Skip the following accounts."),
@@ -35,7 +38,36 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         totals = {}
 
-        for trans in models.Transaction.objects.all():
+        if options['start_date'] is None:
+            start_date = datetime.now().replace(day=1)
+        else:
+            start_date = datetime.strptime(options['start_date'], "%Y-%m-%d")
+
+        start_date = start_date.replace(hour=0, minute=0, second=0, microsecond=0)
+
+        if options['end_date'] is None:
+            try:
+                end_date = start_date.replace(day=31)
+            except ValueError:
+                try:
+                    end_date = start_date.replace(day=30)
+                except ValueError:
+                    try:
+                        end_date = start_date.replace(day=29)
+                    except ValueError:
+                        end_date = start_date.replace(day=28)
+        else:
+            end_date = datetime.strptime(options['end_date'], "%Y-%m-%d")
+
+        end_date = end_date.replace(hour=23, minute=59, second=59, microsecond=999999)
+
+        print start_date, end_date
+
+        transactions = models.Transaction.objects.all().filter(
+            imported_entered_date__gte=start_date,
+            imported_entered_date__lte=end_date,
+            )
+        for trans in transactions:
             if trans.account_filter(exclude=options['accounts']):
                 print "Skipping as in excluded account:", trans
                 continue
@@ -53,12 +85,13 @@ class Command(BaseCommand):
             while i < len(bits):
                 category = "/".join(bits[0:i+1])
                 totals[category] = totals.get(category, 0) + trans.imported_amount
-                print trans, category
 
                 i += 1
 
         i = 0
         while i < 5:
+            if i > options['level']:
+                break
             for category, amount in sorted(totals.items()):
                 level = len(category.split('/'))
                 if level > i:
